4ce7848309b0183618a24be8d2a26d05d7b6330f
Jvesus89 <jvjesus89@gmail.com>
2025-04-08 08:38:05 -0300

    Ajuste branch

--- conectionBase.js
+++ conectionBase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,25 +18,3 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 
-=======
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Configuração do Firebase
-var firebaseConfig = {
-  apiKey: "AIzaSyBbx2ggravKAAwq_QFM1WIHHV82259GaP4",
-  authDomain: "gf-team-542b5.firebaseapp.com",
-  projectId: "gf-team-542b5",
-  storageBucket: "gf-team-542b5.appspot.com",
-  messagingSenderId: "877453744574",
-  appId: "1:877453744574:web:ade69975cc4aec49d181ef"
-};
-
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Torna a instância do Firestore acessível globalmente
-const db = firebase.firestore();
-
-
->>>>>>> 180c373a644b3bc3286fabf75502f422c30ee03e
